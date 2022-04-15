25ea86560255256553fa6fc95055f49ec512dd36
Niedritis <niedritis@inbox.lv>
2022-04-15 18:41:48 +0300

    lidsz 6

--- Majasdarbs/Majasdarbs/Program.cs
+++ Majasdarbs/Majasdarbs/Program.cs
@@ -1,5 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//// See https://aka.ms/new-console-template for more information
 
+//Console.WriteLine("1.uzdevums");
+//Console.WriteLine();
 
 //Console.WriteLine("Kā Tevi sauc?");
 //string username = Console.ReadLine();
@@ -11,6 +13,9 @@
 //Console.WriteLine("=====================");
 //Console.WriteLine();
 
+
+//Console.WriteLine("2.uzdevums");
+//Console.WriteLine();
 
 //Console.WriteLine("Kāds ir Tavs vecums?");
 //string userage = Console.ReadLine();
@@ -24,40 +29,115 @@
 //Console.WriteLine();
 
 
-Console.WriteLine("Ievadi skaitli");
-string number1 = Console.ReadLine();
-int usernumber1 = int.Parse(number1);
-Console.WriteLine("Ievadi otru skaitli");
-string number2 = Console.ReadLine();
-int usernumber2 = int.Parse(number2);
-int Largest1 = Math.Max(usernumber1, usernumber2);
-Console.WriteLine("Lielākais skaitlis ir "+ Largest1);
-int Smollest2 = Math.Min(usernumber1, usernumber2);
-Console.WriteLine("Mazākais skaitlis ir " + Smollest2);
-int leftafter = usernumber1 % usernumber2;
+//Console.WriteLine("3.uzdevums");
+//Console.WriteLine();
+
+//Console.WriteLine("Ievadi pirmo skaitli");
+//string number1 = Console.ReadLine();
+//int usernumber1 = int.Parse(number1);
+//Console.WriteLine("Ievadi otro skaitli");
+//string number2 = Console.ReadLine();
+//int usernumber2 = int.Parse(number2);
+//Console.WriteLine("Ievadi trešo skaitli");
+//string number3 = Console.ReadLine();
+//int usernumber3 = int.Parse(number3);
+//Console.WriteLine("Ievadi ceturto skaitli");
+//string number4 = Console.ReadLine();
+//int usernumber4 = int.Parse(number4);
+
+//int Largest1 = Math.Max(usernumber1, usernumber2);
+//int Largest2 = Math.Max(usernumber3, usernumber4);
+//int Largest3 = Math.Max(Largest1, Largest2);
+
+//Console.WriteLine("Lielākais skaitlis ir "+ Largest3);
+
+
+//Console.WriteLine();
+//Console.WriteLine("=====================");
+//Console.WriteLine();
+
+
+//Console.WriteLine("4.uzdevums");
+//Console.WriteLine();
+
+//Console.WriteLine("Ievadi pirmo skaitli");
+//string numberM1 = Console.ReadLine();
+//int usernumberM1 = int.Parse(numberM1);
+//Console.WriteLine("Ievadi otro skaitli");
+//string numberM2 = Console.ReadLine();
+//int usernumberM2 = int.Parse(numberM2);
+//Console.WriteLine("Ievadi trešo skaitli");
+//string numberM3 = Console.ReadLine();
+//int usernumberM3 = int.Parse(numberM3);
+//Console.WriteLine("Ievadi ceturto skaitli");
+//string numberM4 = Console.ReadLine();
+//int usernumberM4 = int.Parse(numberM4);
+
+//int SMALL1 = Math.Min(usernumberM1, usernumberM2);
+//int SMALL2 = Math.Min(usernumberM3, usernumberM4);
+//int SMALL3 = Math.Min(SMALL1, SMALL2);
+
+//Console.WriteLine("Mazākais skaitlis ir " + SMALL3);
+
+
+Console.WriteLine();
+Console.WriteLine("=====================");
+Console.WriteLine();
+
+
+Console.WriteLine("5.uzdevums");
+Console.WriteLine();
+
+
+Console.WriteLine("Ievadi pirmo skaitli");
+string number51 = Console.ReadLine();
+int usernumber51 = int.Parse(number51);
+Console.WriteLine("Ievadi otro skaitli");
+string number52 = Console.ReadLine();
+int usernumber52 = int.Parse(number52);
+int leftafter = usernumber51 % usernumber52;
 Console.WriteLine("Dalijuma atlikums ir " + leftafter);
 
 
+//Console.WriteLine("Mazākais skaitlis ir " + Smollest2);
+//int leftafter = usernumber1 % usernumber2;
+//Console.WriteLine("Dalijuma atlikums ir " + leftafter);
 
 
-//int result = usernumber + 2;
-//console.writeline("tavs skaitlis palielinats par 2 =   " + result);
+//Console.WriteLine();
+//Console.WriteLine("=====================");
+//Console.WriteLine();
+
+//Console.WriteLine("6.uzdevums");
+
+//Console.WriteLine("Ievadi skaitli");
+//string number6 = Console.ReadLine();
+//bool usernumber6 = int.Parse(number6);
+//Console.WriteLine("Ievadi otru skaitli");
 
 
 
 
 
 
-//Console.WriteLine("Hello, World!");
-//string greeting = "Hello te Arnis";
-//int number = 10;
-//double decimalnumber = 15.5;
-//char symbol = '@';
-//bool answer = true;
+////int result = usernumber + 2;
+////console.writeline("tavs skaitlis palielinats par 2 =   " + result);
 
-//int num1 = 5;
-//int num2 = 2;
-//int sum = num1 + num2;
+
+
+
+
+
+////Console.WriteLine("Hello, World!");
+////string greeting = "Hello te Arnis";
+////int number = 10;
+////double decimalnumber = 15.5;
+////char symbol = '@';
+////bool answer = true;
+
+////int num1 = 5;
+////int num2 = 2;
+////int sum = num1 + num2;
 //double divide = num1 / num2;
 //double convertedNum2 = (double)num2;
 //int leftafter = num1 % num2;
